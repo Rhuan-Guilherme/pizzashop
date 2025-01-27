b548cc5b4608b4e3fa14f56fa8fbcb0e44fb7add
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-01-27 17:57:13 -0300

    Configuração do React Router Dom (Versão V6)

---
+++ src/pages/auth/sing-in.tsx
@@ -0,0 +1,3 @@
+export function SingIn() {
+  return <div>SingIn</div>
+}
---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Dashboard } from './pages/app/dashboard'
+import { SingIn } from './pages/auth/sing-in'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+  },
+  {
+    path: '/singin',
+    element: <SingIn />,
+  },
+])
