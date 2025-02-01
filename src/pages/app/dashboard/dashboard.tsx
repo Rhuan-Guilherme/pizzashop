4aa5b1ffe6e5dac6a5fe023c2f0beec9ea107733
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-02-01 13:36:43 -0300

    Criação de cards para o dashborad

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet-async'
+
+import { DayOrdersAmmountCard } from './day-orders-ammount-card'
+import { MonthCanceledOrdersAmmountCard } from './month-canceled-orders-ammount-card'
+import { MonthOrdersAmmountCard } from './month-orders-ammount-card'
+import { MonthRevenueCard } from './month-revenue-card'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmmountCard />
+          <DayOrdersAmmountCard />
+          <MonthCanceledOrdersAmmountCard />
+        </div>
+      </div>
+    </>
+  )
+}
