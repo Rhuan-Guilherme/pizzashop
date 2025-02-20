dbca9e84b2cee95856d78f9eefb21b257b461bd9
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-02-20 19:25:52 -0300

    Adicionando métricas do Dashboard

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,8 +1,33 @@
-import { Outlet } from 'react-router-dom'
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components/Header'
+import { api } from '@/lib/axios'
 
 export function AppLayout() {
+  const navvigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (reponse) => reponse,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navvigate('/sing-in', { replace: true })
+          }
+        }
+      },
+    )
+
+    return () => {
+      api.interceptors.request.eject(interceptorId)
+    }
+  }, [navvigate])
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
